84001a98a029a11a41571e5fcad7192dc70a1249
chloehjung <37277782+chloehjung@users.noreply.github.com>
2022-03-14 23:40:19 +1300

    Style navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,22 @@
 import React from "react";
 import {Link} from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Navigation = ({userObj}) => <nav>
-    <ul>
-        <li><Link to="/">Home</Link></li>
+    <ul className="navigation">
+        <li>
+            <Link to="/">
+                <FontAwesomeIcon
+                    icon={faTwitter}
+                    color={"#04AAFF"}
+                    size="3x"
+                    style={{ marginBottom: 30 }}
+                />
+            </Link>
+        </li>
         <li><Link to="/profile">{userObj && userObj.displayName}의 Profile</Link></li>
     </ul>
 </nav>
